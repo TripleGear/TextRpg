2882f73d396fe8346d9c16d42c910cce3296b9ab
Marsik-dev <61378924+Marsik-dev@users.noreply.github.com>
2023-05-21 11:26:03 +0300

    1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "TripleGear-TextRPG-0.0.1.3";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/Build.framework.js.unityweb",
+    "Build/Build.data.unityweb",
+    "Build/Build.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
